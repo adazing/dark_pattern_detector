9bd1f0ac1bce8cc986974dd1ebc761aa7e180e05
adazing <adalangford123@gmail.com>
2024-09-21 22:42:37 -0400

    fixed allowed hosts

--- detector/detector/settings.py
+++ detector/detector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ldqj31e561a%*fs$e(bdd()+49kb)x4cse1zxtf!^-@4$8783^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
