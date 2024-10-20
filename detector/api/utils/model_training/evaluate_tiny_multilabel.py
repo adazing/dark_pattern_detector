8170903e9de52bd2bc0e03d4597a7f6b2c735ad8
adazing <adalangford123@gmail.com>
2024-10-20 11:18:45 -0400

    fixed evaluation

--- detector/api/utils/model_training/evaluate_tiny_multilabel.py
+++ detector/api/utils/model_training/evaluate_tiny_multilabel.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43e8d8a35f74fd296f1f77a7d491253db5d04fc4f32c4245cedb66a50fe37537
+oid sha256:4b9fa4768cdcc9f6ccf9102ce4225243e2738a38c683aa1ad9fd7894670aa219
 size 2433
